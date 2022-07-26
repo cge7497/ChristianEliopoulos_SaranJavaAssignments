ad101db949433d69b5dc3b7c64338122bea01b2b
Christian Eliopoulos <cge7497@rit.edu>
2022-07-26 16:36:46 -0400

    Optimizes A5 Code/Completes Tuesday's Assignment

--- A5/src/main/java/com/hcl/A5/A5.java
+++ A5/src/main/java/com/hcl/A5/A5.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -53,7 +55,8 @@ class AgeComparator implements Comparator {
 
 public class A5 {
 
-	static int totalAgeM = 0; static int totalAgeF = 0;
+	static int totalAgeM = 0;
+	static int totalAgeF = 0;
 
 	public static void main(String[] args) {
 		Map<Employee, Integer> empMap = new HashMap<>();
@@ -68,79 +71,108 @@ public class A5 {
 		empMap.put(new Employee(8, 24, 2022, 70000, "Jack", "Male", "IT"), 50);
 		empMap.put(new Employee(9, 62, 2010, 52000, "Isaac", "Male", "Maintenance"), 97);
 		empMap.put(new Employee(10, 64, 2001, 65000, "Maria", "Female", "IT"), 32);
-
+		empMap.put(new Employee(11, 47, 2009, 55000, "Mary", "Female", "Maintenance"), 47);
+		
 		// Prompt 1
 		{
-			// I got this idea for stream.filter from https://stackoverflow.com/a/51686487
-			long maleCount = empMap.keySet().stream().filter(e -> e.gender == "Male").count();
-			long femaleCount = empMap.keySet().stream().filter(e -> e.gender == "Female").count();
 
-			System.out.println("There are " + maleCount + " male and " + femaleCount + " female employees.");
+			Map<String, Long> mAndFGender = empMap.keySet().stream()
+					.collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+
+			System.out.println("Employee count: " + mAndFGender);
+
+			/* Old method
+			 * I got this idea for stream.filter from https://stackoverflow.com/a/51686487
+			 * long maleCount = empMap.keySet().stream().filter(e -> e.gender == "Male").count(); 
+			 * long femaleCount = empMap.keySet().stream().filter(e -> e.gender == "Female").count(); 
+			 * // System.out.println("There are " + maleCount + " male and " + femaleCount + " female employees.");
+			 */
 		}
 		// Prompt 2
 		{
-			empMap.forEach((emp, val) -> {
-				if (emp.gender == "Male")	totalAgeM += emp.age;
-				else if (emp.gender == "Female") totalAgeF += emp.age;
-			});
-			double avgAgeM = totalAgeM / empMap.keySet().stream().filter(e -> e.gender == "Male").count();
-			double avgAgeF = totalAgeF/ empMap.keySet().stream().filter(e -> e.gender == "Female").count();
-			System.out.println(String.format("\nThe average men's age is %.2f and the average women's age is %.2f", avgAgeM, avgAgeF));
+			Map<String, Double> mAndFAge = empMap.keySet().stream()
+					.collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
+
+			System.out.println("\nAverage Age: " + mAndFAge);
+			
+			/* Old method
+			 * empMap.forEach((emp, val) -> { 
+			 * if (emp.gender == "Male") totalAgeM += emp.age; 
+			 * else if (emp.gender == "Female") totalAgeF += emp.age; }); 
+			 * double avgAgeM = totalAgeM / empMap.keySet().stream().filter(e -> e.gender == "Male").count(); 
+			 * double avgAgeF = totalAgeF/ empMap.keySet().stream().filter(e -> e.gender == "Female").count();
+			 * System.out.println(String.format("\nThe average men's age is %.2f and the average women's age is %.2f", avgAgeM, avgAgeF));
+			 */
 		}
 		// Prompt 3
 		{
-			TreeMap<Employee, Integer> salMap = new TreeMap<Employee, Integer>(new SalComparator());
-			salMap.putAll(empMap);
-			final Employee highestPaid = salMap.lastKey();
-			System.out
-					.println("\nThe highest paid employee is " + highestPaid.name + " who is paid $" + highestPaid.salary);
+			Employee highestPaid = empMap.keySet().stream()
+					.collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary))).orElse(null);
+			/*
+			 * TreeMap<Employee, Integer> salMap = new TreeMap<Employee, Integer>(new SalComparator()); 
+			 * salMap.putAll(empMap); 
+			 * final Employee highestPaid = salMap.lastKey();
+			 */
+			System.out.println(
+					"\nThe highest paid employee is " + highestPaid.name + " who is paid $" + highestPaid.salary);
+
 		}
 		// Prompt 4
 		{
 			System.out.print("\nEmployees who have joined after 2015: ");
-			empMap.keySet().stream().filter(e -> e.yearOfJoining > 2015).forEach((e) -> System.out.print(e.name + ", "));
+			empMap.keySet().stream().filter(e -> e.yearOfJoining > 2015)
+					.forEach((e) -> System.out.print(e.name + ", "));
 		}
 		// Prompt 5
 		{
-			TreeMap<Employee, Integer> ageMap = new TreeMap<Employee, Integer>((e1, e2) -> {
-				if (e1.age == e2.age) {
-					return 0;
-				} else if (e1.age > e2.age) {
-					return 1;
-				} else
-					return -1;
-			});
-			ageMap.putAll(empMap);
-
-			final Employee oldest = ageMap.lastKey();
-			System.out.println("\n\nThe most senior employee is " + oldest.name + " who is " + oldest.age + " years old.");
+			Employee oldest = empMap.keySet().stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getAge)))
+					.orElse(null);
+			/*
+			 * TreeMap<Employee, Integer> ageMap = new TreeMap<Employee, Integer>((e1, e2) -> { 
+			 * if (e1.age == e2.age) { return 0; } else if (e1.age > e2.age) { return
+			 * 1; } else return -1; }); 
+			 * ageMap.putAll(empMap);
+			 * final Employee oldest = ageMap.lastKey();
+			 */
+			System.out.println(
+					"\n\nThe most senior employee is " + oldest.name + " who is " + oldest.age + " years old.");
 		}
 		// Prompt 6
 		{
-			long maintenanceCount = empMap.keySet().stream().filter(e -> e.dept == "Maintenance").count();
-			long r_and_d_Count = empMap.keySet().stream().filter(e -> e.dept == "R & D").count();
-			long HRCount = empMap.keySet().stream().filter(e -> e.dept == "HR").count();
-			long ITCount = empMap.keySet().stream().filter(e -> e.dept == "IT").count();
-			System.out.println(String.format("\nEmployees per department: Maintenance: %d, R&D: %d, HR: %d, IT: %d",
-					maintenanceCount, r_and_d_Count, HRCount, ITCount));
+			Map<String, Long> perDept = empMap.keySet().stream()
+					.collect(Collectors.groupingBy(Employee::getDept, Collectors.counting()));
+
+			System.out.println("\nEmployees per department: " + perDept);
+
+			/*
+			 * long maintenanceCount = empMap.keySet().stream().filter(e -> e.dept == "Maintenance").count(); 
+			 * long r_and_d_Count = empMap.keySet().stream().filter(e -> e.dept == "R & D").count(); 
+			 * long HRCount = empMap.keySet().stream().filter(e -> e.dept == "HR").count(); 
+			 * long ITCount = empMap.keySet().stream().filter(e -> e.dept == "IT").count();
+			 */
 		}
 		// Prompt 7
 		{
-			long maintenance_Male_Count = empMap.keySet().stream()
-					.filter(e -> e.dept == "Maintenance" && e.gender == "Male").count();
-			long maintenance_Female_Count = empMap.keySet().stream()
-					.filter(e -> e.dept == "Maintenance" && e.gender == "Female").count();
-			System.out.println(String.format("\nMale and Female maintenance employees: %d male and %d female",
-					maintenance_Male_Count, maintenance_Female_Count));
+			Map<String, Long> maintenance_Gender_Count = empMap.keySet().stream().filter(e -> e.dept == "Maintenance")
+					.collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+
+			System.out.println("Maintenance employees: " + maintenance_Gender_Count);
+
+			/*
+			 * long maintenance_Male_Count = empMap.keySet().stream() .filter(e -> e.dept == "Maintenance" && e.gender == "Male").count(); 
+			 * long maintenance_Female_Count = empMap.keySet().stream() .filter(e -> e.dept == "Maintenance" && e.gender == "Female").count();
+			 */
+			// System.out.println(String.format("\nMale and Female maintenance employees: %d male and %d female", maintenance_Male_Count, maintenance_Female_Count));
 		}
 		// Prompt 8
 		{
-			Iterator<Employee> itr = empMap.keySet().iterator();
-			int totalSalary = 0;
-			while (itr.hasNext()) {
-				totalSalary += itr.next().salary;
-			}
-			double avg = totalSalary / empMap.size();
+			double avg = empMap.keySet().stream().collect(Collectors.averagingInt(Employee::getSalary));
+			/*
+			 * Iterator<Employee> itr = empMap.keySet().iterator(); 
+			 * int totalSalary = 0;
+			 * while (itr.hasNext()) { totalSalary += itr.next().salary; } 
+			 * double avg = totalSalary / empMap.size();
+			 */
 			System.out.println(String.format("\nThe average salary is $%1$,.2f", avg));
 		}
 		// Prompt 9
@@ -154,18 +186,17 @@ public class A5 {
 		// Prompt 10
 		{
 			System.out.println("\n");
+			Map<String, Long> byDept = empMap.keySet().stream()
+					.collect(Collectors.groupingBy(Employee::getDept,Collectors.counting()));
 
-			System.out.print("Employees in R & D: ");
-			empMap.keySet().stream().filter((e) -> e.dept == "R & D").forEach((e) -> System.out.print(e.name + ", "));
-
-			System.out.print("\nEmployees in Maintenance: ");
-			empMap.keySet().stream().filter((e) -> e.dept == "Maintenance").forEach((e) -> System.out.print(e.name + ", "));
-		
-			System.out.print("\nEmployees in HR: ");
-			empMap.keySet().stream().filter((e) -> e.dept == "HR").forEach((e) -> System.out.print(e.name + ", "));
-
-			System.out.print("\nEmployees in IT: ");
-			empMap.keySet().stream().filter((e) -> e.dept == "IT").forEach((e) -> System.out.print(e.name + ", "));
+			/*
+			 * System.out.print("Employees in R & D: "); empMap.keySet().stream().filter((e) -> e.dept == "R & D").forEach((e) -> System.out.print(e.name + ", "));
+			 * System.out.print("\nEmployees in Maintenance: ");
+			 * empMap.keySet().stream().filter((e) -> e.dept == "Maintenance") .forEach((e)-> System.out.print(e.name + ", "));
+			 * System.out.print("\nEmployees in HR: "); empMap.keySet().stream().filter((e)-> e.dept == "HR").forEach((e) -> System.out.print(e.name + ", "));
+			 * System.out.print("\nEmployees in IT: "); empMap.keySet().stream().filter((e) -> e.dept == "IT").forEach((e) -> System.out.print(e.name + ", "));
+			 */
 		}
+
 	}
 }
